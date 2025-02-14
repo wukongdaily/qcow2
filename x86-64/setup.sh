868b9a2c8f2ef89365e896416618fe3369383560
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2025-02-14 13:44:47 +0800

    Update setup.sh

--- x86-64/setup.sh
+++ x86-64/setup.sh
@@ -50,7 +50,7 @@ fi
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
 echo "$PACKAGES"
 
-make image PROFILE="generic" PACKAGES="$PACKAGES" FILES="/builder/files" ROOTFS_PARTSIZE=$PROFILE
+make image PACKAGES="$PACKAGES" FILES="/builder/files" ROOTFS_PARTSIZE=$PROFILE
 
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
